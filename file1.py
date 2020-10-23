45bc0f69b650c058f7d7a4bc4b8f1fb43e0daa83
artyomka998 <artyomka998@gmail.com>
2020-10-23 20:02:46 +0700

    Создание конструктора класса

--- file1.py
+++ file1.py
@@ -2,6 +2,14 @@ print("Введите количество клиентов")
 a=int(input())
 
 class work
-	def unit(okna,vremya,raboti,tovari)
+	def __init__(okna,vremya,raboti,tovari)
 		self.rb=raboti
+		self.ok=okna
+		self.vr=vremya
+		self.tv=tovari
+	def display_info(self):
+		print("Среднее количество выполняемых работ в час:",self.rb)
+		print("Количество окон для обслуживания:",self.ok)
+		print("Среднее время обслуживания одного клиента:",self.vr)
+		print("Среднее количество продаваемых товаров в час:,self.tv)
 
