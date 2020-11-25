a015857e15a7cb5ab6d6f50461854fd09d21ba28
artyomka998 <artyomka998@gmail.com>
2020-11-25 15:17:09 +0700

    Исправление ошибок

--- file1.py
+++ file1.py
@@ -1,9 +1,11 @@
 class work:
-	def __init__(okna,vremya,raboti,tovari):
+	def __init__(self,okna,vremya,raboti,tovari):
 		self.rb=raboti
 		self.ok=okna
 		self.vr=vremya
 		self.tv=tovari
+	def __return__(self):
+		return "\n Количество работ в час = {} \n Количество окон = {} \n Время обслуживания одного клиента = {} \n  Продаваемых товаров в час = {}".format (self.rb,self.ok,self.vr,self.tv)
 	def display_info(self):
 		print("Среднее количество выполняемых работ в час:",self.rb)
 		print("Количество окон для обслуживания:",self.ok)
@@ -16,13 +18,15 @@ class work:
 		self.bolsheokon=self.ok + 1
 		self.rb=self.rb * 1.33 
 		self.virab=self.rb // self.vr	
-		return "\n Выработка предприятия за время %s составляет %s" % (self.vr,self,virab)
-	exmpl1=work("Установка нового рабочего окна", 3,"Количество выполняемых работ в день", 30)
-	exmpl2=work("Увеличено количество рабочих окон", 4,"Количество выполняемых работ в день",40)
-	print (exmpl1.virab())
-	print (exmpl1.bolsheokon())
-	elist=list()
-	elist.append(exmpl1)
-	elist.append(exmpl2)
-	print(elist)
+		return "\n Выработка предприятия за время %s составляет %s" % (self.vr,self.virab)
+exmpl1 = work("Установка нового рабочего окна", 3,"Количество выполняемых работ в день", 30)
+exmpl2 = work("Увеличено количество рабочих окон", 4,"Количество выполняемых работ в день",40)
+print (exmpl1.virab())
+print (exmpl1.bolsheokon())
+print (exmpl2.virab())
+print (exmpl2.bolsheokon())
+elist=list()
+elist.append(exmpl1)
+elist.append(exmpl2)
+print(elist)
 	
