5f6c1fb370bdd184921e4dacad1792eac4249445
artyomka998 <artyomka998@gmail.com>
2020-11-26 22:41:04 +0700

    Корректировка переменных

--- file1.py
+++ file1.py
@@ -1,8 +1,8 @@
 class work:
 	def __init__(self,okna,vremya,raboti,tovari):
-		self.rb=raboti
 		self.ok=okna
 		self.vr=vremya
+		self.rb=raboti
 		self.tv=tovari
 	def __return__(self):
 		return "\n Количество работ в час = {} \n Количество окон = {} \n Время обслуживания одного клиента = {} \n  Продаваемых товаров в час = {}".format (self.rb,self.ok,self.vr,self.tv)
@@ -13,20 +13,17 @@ class work:
 		print("Среднее количество продаваемых товаров в час:",self.tv)
 	def virab(self):
 		self.virab=self.rb // self.vr
-		return "\n Выработка предприятия за время %s составляет %s" % (self.vr,self.virab)
+		return "\n Выработка предприятия за %s часа(ов) составляет %s" % (self.vr,self.virab)
 	def bolsheokon(self):
 		self.bolsheokon=self.ok + 1
 		self.rb=self.rb * 1.33 
 		self.virab=self.rb // self.vr	
-		return "\n Выработка предприятия за время %s составляет %s" % (self.vr,self.virab)
-exmpl1 = work("Установка нового рабочего окна", 3,"Количество выполняемых работ в день", 30)
-exmpl2 = work("Увеличено количество рабочих окон", 4,"Количество выполняемых работ в день",40)
-print (exmpl1.virab())
-print (exmpl1.bolsheokon())
-print (exmpl2.virab())
-print (exmpl2.bolsheokon())
-elist=list()
-elist.append(exmpl1)
-elist.append(exmpl2)
-print(elist)
+		return "\n Выработка предприятия за %s часа(ов) составляет %s" % (self.vr,self.virab)
+a1 = work(8, 5,60, 81)
+a2 = work(12, 8,97,169)
+print (a1.virab())
+print (a1.bolsheokon())
+print (a2.virab())
+print (a2.bolsheokon())
+
 	
