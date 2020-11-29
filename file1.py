8d1cee134e4e0b11be3b72c708846f9d8773215d
artyomka998 <artyomka998@gmail.com>
2020-11-29 18:46:34 +0700

    Лабораторная 3

--- file1.py
+++ file1.py
@@ -10,10 +10,6 @@ class work:
         self.data_csv_dict = []
         self.data_csv_list = []
 
-    def __return__(self):
-        return "\n Количество работ в час = {} \n Количество окон = {} \n Время обслуживания одного клиента = {} \n  Продаваемых товаров в час = {}".format(
-            self.rb, self.ok, self.vr, self.tv)
-
     def display_info(self):
         print("Среднее количество выполняемых работ в час:", self.rb)
         print("Количество окон для обслуживания:", self.ok)
@@ -44,11 +40,35 @@ class work:
                 self.data_csv_list.append(row)
             print(self.data_csv_list)
 
+    def write_csv_variant1(self, filename):
+        fieldnames = []
+        for data in self.data_csv_dict:
+            for field in data:
+                fieldnames.append(field)
+
+        with open(filename, 'w') as file:
+            writer = csv.DictWriter(file, delimiter=';', fieldnames=fieldnames)
+            writer.writeheader()
+            for row in self.data_csv_dict:
+                writer.writerow(row)
+
+    def write_csv_variant2(self, filename):
+        with open(filename, 'w') as file:
+            writer = csv.writer(file, delimiter=';')
+            for row in self.data_csv_list:
+                writer.writerow(row)
+
 
 a1 = work(8, 5, 60, 81)
 a1.read_csv_variant1('lab3.csv')
+a1.read_csv_variant2('lab3.csv')
+a1.write_csv_variant1('test1.csv')
+a1.write_csv_variant2('test2.csv')
 a2 = work(12, 8, 97, 169)
 a2.read_csv_variant2('lab3.1.csv')  
+a2.read_csv_variant1('lab3.1.csv')
+a2.write_csv_variant2('a2_test1.csv')
+a2.write_csv_variant1('a2_test2.csv')
 print(a1.virab())
 print(a1.bolsheokon())
 print(a2.virab())
