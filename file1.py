aae361aa34868de6d913c3bb9c7df41956678c32
artyomka998 <artyomka998@gmail.com>
2020-11-30 15:23:50 +0700

    Запрос для выборки данных из таблицы

--- file1.py
+++ file1.py
@@ -63,7 +63,9 @@ class work:
 cursor.execute("""CREATE TABLE Predpriyatie (name text, okna integer, vremya integer, raboti integer, tovari integer) """)
 Predpriyatie = [('Pered Avtomatization', '8','5','60','81'),('Posle Avtmzn','12','8','97','169')]
 cursor.executemany("INSERT INTO Predpriyatie VALUES (?,?,?,?,?)", Predpriyatie)
-
+sql = "SELECT * FROM Predpriyatie"
+cursor.execute(sql)
+print(cursor.fetchall())
 
 
 a1 = work(8, 5, 60, 81)
