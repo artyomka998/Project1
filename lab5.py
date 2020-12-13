07dbc4916c5d5a227344db6bd423b43994e71ea0
artyomka998 <artyomka998@gmail.com>
2020-12-13 15:04:30 +0700

    Создание интерйеса через qt designer

--- lab5.py
+++ lab5.py
@@ -1,14 +1,11 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
 
-if __name__ == '__main__':
+Form, Window = uic.loadUiType("lab5.ui")
 
-    app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(1000, 700)
-    w.move(250, 70)
-    w.setWindowTitle('Лабораторная 5')
-    w.show()
-
-    sys.exit(app.exec_())
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec_()
